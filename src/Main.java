52764424659303551b7ab324c994b8b761a66c94
“Vladimir_Grin” <mortywallet@gmail.com>
2023-08-18 23:24:51 +0300

    gitQA3

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,17 @@ public class Main {
     public static void main(String[] args) {
         int wallet = 150;
         int bonus;
-        int result;
-        int deposit = 760;
-        int replenished_wallet;
-        replenished_wallet = wallet + deposit;
-        if (deposit >= 1000) {
+        int deposit = 1100;
+        int replenishedWallet;
+        replenishedWallet = wallet + deposit;
+        if (deposit > 1000) {
             bonus = deposit / 100;
-            System.out.println("Ваш счет составляет " + replenished_wallet + " , вам начислено " + bonus + " бонусов");
-            System.out.println("Общая сумма составляет " + (replenished_wallet + bonus));
+            System.out.println("Ваш счет составляет " + replenishedWallet + " , вам начислено " + bonus + " бонусов");
+            System.out.println("Общая сумма составляет " + (replenishedWallet + bonus));
         } else {
             bonus = 0;
-            System.out.println("Ваш счет составляет " + replenished_wallet + " , вам начислено " + bonus + " бонусов");
-            System.out.println("Общая сумма составляет " + (replenished_wallet + bonus));
+            System.out.println("Ваш счет составляет " + replenishedWallet + " , вам начислено " + bonus + " бонусов");
+            System.out.println("Общая сумма составляет " + (replenishedWallet + bonus));
         }
 
     }
